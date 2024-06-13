90594627aa83581d8a44569d33329509d44f9d3e
RaghadShalabi <rgdshb@gmail.com>
2024-06-13 21:41:54 +0300

    redirect to sign in from rescue team confirm email

--- src/modules/auth/controller/auth.controller.js
+++ src/modules/auth/controller/auth.controller.js
@@ -169,7 +169,7 @@ export const confirmEmail = async (req, res, next) => {
 
   // Check if the user is a RescueTeam and redirect them to the sign-in page
   if (user.role === "RescueTeam") {
-    return res.redirect(process.env.SIGN_IN);
+    return res.redirect('https://iamalive-front.onrender.com/');
   }
 
   return res
